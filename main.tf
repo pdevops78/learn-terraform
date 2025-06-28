485a716b03b40e1e7c78c132d4bd26059d2ec420
Your Name <vemasani.pavani@gmail.com>
2025-06-28 14:10:02 +0530

    provisioner

--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ vault_token = var.vault_token
 env  = var.env
 instance_type = var.instance_type
 component = "frontend"
+zone_id =var.zone_id
 }
--- module/app/variable.tf
+++ module/app/variable.tf
@@ -3,4 +3,5 @@ variable "env"{}
 
 variable "instance_type"{}
 variable "component"{}
+variable "zone_id"{}
 
